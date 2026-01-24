a8a6b5b2aa7b4865d22d2f31c505f7c398ee25cf
Ayanrocks <ayanb1999@gmail.com>
2026-01-24 15:52:25 +0530

    Refactored config logic and shifted to config package to reduce coupling

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,6 +1,14 @@
 package config
 
-import "github.com/spf13/cobra"
+import (
+	"fmt"
+
+	"github.com/pelletier/go-toml/v2"
+	"github.com/spf13/cobra"
+
+	"mneme/internal/core"
+	"mneme/internal/logger"
+)
 
 var ConfigCmd = &cobra.Command{
 	Use:   "config",
@@ -17,4 +25,51 @@ var showCmd = &cobra.Command{
 
 func init() {
 	ConfigCmd.AddCommand(showCmd)
+}
+
+// DefaultConfig is the default configuration for mneme
+var DefaultConfig = core.DefaultConfig{
+	Version: 1,
+	Index: core.IndexConfig{
+		SegmentSize:          500,
+		MaxTokensPerDocument: 10000,
+		ReindexOnModify:      true,
+		SkipBinaryFiles:      true,
+	},
+	Sources: core.SourcesConfig{
+		Paths:             []string{},
+		IncludeExtensions: []string{},
+		Ignore:            []string{".git", "node_modules", ".vscode", ".idea", "vendor", ".cache"},
+	},
+	Watcher: core.WatcherConfig{
+		Enabled:    true,
+		DebounceMS: 500,
+	},
+	Search: core.SearchConfig{
+		DefaultLimit: 20,
+		UseStopwords: true,
+		Language:     "en",
+	},
+	Ranking: core.RankingConfig{
+		TFIDFWeight:         1,
+		RecencyWeight:       0.3,
+		TitleBoost:          1.5,
+		PathBoost:           1.2,
+		RecencyHalfLifeDays: 30,
+	},
+	Logging: core.LoggingConfig{
+		Level: "info",
+		JSON:  true,
+	},
+}
+
+// DefaultConfigWriter returns the default configuration as a TOML string
+func DefaultConfigWriter() (string, error) {
+	configBytes, err := toml.Marshal(DefaultConfig)
+	if err != nil {
+		logger.Errorf("Error marshaling default config to TOML: %+v", err)
+		return "", fmt.Errorf("failed to marshal default config: %w", err)
+	}
+
+	return string(configBytes), nil
 }
